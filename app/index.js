2a5df90ef72c3d8039602555b38af17b28f4ca3d
Saman Manesh <samanmanesh7@gmail.com>
2021-09-01 11:17:38 -0400

    pupulate user works/ had to send get reqest in index(app) to run cron "Play"

--- app/index.js
+++ app/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,10 +20,11 @@ class App {
         this._context = new CoinBotContext_1.default();
         this.setupMiddleware();
         this.setupRoutes(routes);
-        this.setupCron();
         this.app.get("/", (req, res) => {
             res.send(`'Hello world'`);
         });
+        this.app.get("/play", this.setupCron.bind(this));
+        // this.setupCron();
     }
     listen(port) {
         this.app.listen(port, () => console.log(`App is listening on port ${port}`));
@@ -28,8 +38,16 @@ class App {
             this.app.use(route.path, route.controller.router);
         });
     }
-    setupCron() {
-        this._context.runCron();
+    setupCron(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield this._context.runCron();
+                res.status(200).send("Cron is running");
+            }
+            catch (err) {
+                console.error({ message: err });
+            }
+        });
     }
 }
 exports.default = App;
